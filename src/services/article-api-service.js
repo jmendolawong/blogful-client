93bdfa1453dcab63c4847b610146da305dc0f43e
mendolawong <jwong.ncsu@gmail.com>
2020-01-25 10:05:21 -0500

    updated to bearer token

---
+++ src/services/article-api-service.js
@@ -0,0 +1,60 @@
+import config from '../config'
+import TokenService from './token-service'
+
+const ArticleApiService = {
+  getArticles() {
+    return fetch(`${config.API_ENDPOINT}/articles`, {
+      headers: {
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getArticle(articleId) {
+    return fetch(`${config.API_ENDPOINT}/articles/${articleId}`, {
+      headers: {
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getArticleComments(articleId) {
+    return fetch(`${config.API_ENDPOINT}/articles/${articleId}/comments`, {
+      headers: {
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postComment(articleId, text) {
+    return fetch(`${config.API_ENDPOINT}/comments`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify({
+        article_id: articleId,
+        text,
+      }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+}
+
+export default ArticleApiService
